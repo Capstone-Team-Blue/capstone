3bc8d1883a835f90e21f558062cd9ac03509f605
harmeetsingh029 <singh.h1995@gmail.com>
2022-01-24 14:37:02 -0600

    finished seeding db

---
+++ univ-boilerplate/build/precache-manifest.ba279fe8e6e9fdccaeece99a66a31737.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8ee4c4f9aede6430521c748f38af990",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37a74bca145e178603c9",
+    "url": "/static/js/2.46712fcd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.46712fcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "168a2bb868dfc7a7c90c",
+    "url": "/static/js/main.7e843ec0.chunk.js"
+  },
+  {
+    "revision": "7e23c3461cd3b1e0abd6",
+    "url": "/static/js/runtime-main.da5551cf.js"
+  }
+]);
