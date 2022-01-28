95611c11e627d1059ac34a88034850d4078be415
Dan_Kempert <dwkempert@gmail.com>
2022-01-28 15:16:30 -0500

    merge conflict

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import {BrowserRouter, Route, Link, Routes} from 'react-router-dom'
 import Login from './Login'
-<<<<<<< HEAD
 import Nav from './Nav'
 import Orders from './Orders'
 
 const App = () => {
-=======
-
-const App = () => {
-
->>>>>>> 1bb0850b3e638413ca0049043d430768e9e2bb46
   const [ isLoggedIn, setIsLoggedIn ] = useState(false)
   const [ loginToken, setLoginToken ] = useState('')
   const [ globalUserId, setGlobalUserId ] = useState('')
@@ -20,7 +14,6 @@ const App = () => {
     <BrowserRouter>
       <div className="App">
         <h1>Hello, World!</h1>
-<<<<<<< HEAD
         <Nav />
         <Route path='/login'>
           <Login setIsLoggedIn={setIsLoggedIn} setLoginToken={setLoginToken} setGlobalUserId={setGlobalUserId}/>
@@ -28,9 +21,6 @@ const App = () => {
         <Route path='/myorders'>
           <Orders globalUserId={globalUserId}/>
         </Route>
-=======
-        <Login setIsLoggedIn={setIsLoggedIn} setLoginToken={setLoginToken} setGlobalUserId={setGlobalUserId}/>
->>>>>>> 1bb0850b3e638413ca0049043d430768e9e2bb46
       </div>
     </BrowserRouter>
   );
