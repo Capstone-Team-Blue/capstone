0b34810c73458a61cff53e16bf70b7c368bc609a
Dan_Kempert <dwkempert@gmail.com>
2022-01-31 16:54:24 -0500

    working orders list for logged in user

--- src/components/OrderDetails.js
+++ src/components/OrderDetails.js
@@ -1,10 +1,42 @@
 import React from "react"
 
 const OrderDetails = (props) => {
+    const { loginToken, myOrders, selectedOrder } = props
+    
+    console.log('selectedOrder:', myOrders[selectedOrder])
+
+    let totalCost = 0
+
+    if (selectedOrder) {
+        myOrders[selectedOrder].products.map((elem) => {
+            totalCost += elem.quantity * elem.unitCost/100
+        })
+    }
+
     return (
-        <div id='orderDetailsContainer'>
-            <h2>Order Details</h2>
-        </div>
+        loginToken ?
+            <div id='orderDetailsContainer'>
+                <h2>Order Details</h2>
+                <div>
+                    {selectedOrder ? myOrders[selectedOrder].products.map((el, idx) => (
+                        <div className="orderDetailsItem" key={idx}>
+                            <div id='detailNameAndId'>
+                                <h3>Product Name: {el.name}</h3><div>id#: {el.productId}</div>
+                            </div>
+                            <div id='detailQuantityAndCosts'>
+                                <div>Quantity: {el.quantity}</div>
+                                <div>Single Unit Cost: ${el.unitCost/100}</div>
+                                <div>Product Total Cost: ${el.quantity * el.unitCost/100}</div>
+                            </div>
+                        </div>
+                    )) : undefined}
+                </div>
+                {selectedOrder ? <div id='orderDetailTotalCost'><h3>Selected Order Total Cost: ${totalCost}</h3></div> : undefined }
+            </div>
+        :
+            <div id='orderDetailsContainer'>
+                <h2>No logged in user</h2>
+            </div>
     )
 }
 
