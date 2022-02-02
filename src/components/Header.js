e0e0d7886579b824dea3a789dc0ee482a44ab055
Dan_Kempert <dwkempert@gmail.com>
2022-02-02 09:39:04 -0500

    fixed props so logout button appears when logged in

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,14 @@ import React from "react";
 import { Link } from 'react-router-dom'
 import Nav from "./Nav.js";
 
-
-
 export default function Header(props) {
+  
+  const { setIsLoggedIn, setLoginToken, setUsername, setPassword, isLoggedIn } = props
+
   return (
     <header className="header">
       <Link to="/"><img className='logo' src={process.env.PUBLIC_URL+"assets/logo.png"}  /></Link>
-      <Nav />
+      <Nav setIsLoggedIn={setIsLoggedIn} setLoginToken={setLoginToken} setUsername={setUsername} setPassword={setPassword} isLoggedIn={isLoggedIn} />
     </header>
   );
 }
