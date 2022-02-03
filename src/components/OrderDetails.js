02c57f82f260a19a1dd00b6b5cfb99922eee0e24
Dan_Kempert <dwkempert@gmail.com>
2022-02-03 11:16:04 -0500

    added message to order details for when there are no products associated with an order number

--- src/components/OrderDetails.js
+++ src/components/OrderDetails.js
@@ -17,22 +17,28 @@ const OrderDetails = (props) => {
             <div id='orderDetailsContainer'>
                 <h2>Order Details</h2>
                 <div>
-                    {selectedOrder ? myOrders[selectedOrder].products.map((el, idx) => (
-                        <div className="orderDetailsItem" key={idx}>
-                            <div id='detailNameAndId'>
-                                <h3>Product Name: {el.name}</h3><div>id#: {el.productId}</div>
+                    {selectedOrder ? 
+                        myOrders[selectedOrder].products[0] ?
+                            myOrders[selectedOrder].products.map((el, idx) => (
+                                <div className="orderDetailsItem" key={idx}>
+                                    <div id='detailNameAndId'>
+                                        <h3>Product Name: {el.name}</h3><div>id#: {el.productId}</div>
+                                    </div>
+                                    <div id='detailQuantityAndCosts'>
+                                        <div>Quantity: {el.quantity}</div>
+                                        <div>Single Unit Cost: ${el.unitCost/100}</div>
+                                        <div>Product Total Cost: ${(el.quantity * el.unitCost)/100}</div>
+                                    </div>
+                                </div>
+                            ))
+                        :
+                            <div className="orderDetailsItem">
+                                <h3>No product information available for that order.</h3>
                             </div>
-                            <div id='detailQuantityAndCosts'>
-                                <div>Quantity: {el.quantity}</div>
-                                <div>Single Unit Cost: ${el.unitCost/100}</div>
-                                <div>Product Total Cost: ${(el.quantity * el.unitCost)/100}</div>
-                            </div>
-                        </div>
-                    )) 
                     : 
-                    <div className="orderDetailsItem" style={{borderBottom: "none"}}>
-                        <h3>Select an order from the list to view the details for that order.</h3>
-                    </div>
+                        <div className="orderDetailsItem" style={{borderBottom: "none"}}>
+                            <h3>Select an order from the list to view the details for that order.</h3>
+                        </div>
                     }
                 </div>
                 {selectedOrder ? <div id='orderDetailTotalCost'><h3>Selected Order Total Cost: ${totalCost}</h3></div> : undefined }
