cb133c7517226eb3ae4cf43cd36cf06b8f6deb28
Dan_Kempert <dwkempert@gmail.com>
2022-02-07 10:55:15 -0500

    latest deploy build

---
+++ build/precache-manifest.fc1d08163d4fa4f8fd1c457108464a26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "439da2c132c1381a8a977ae5bfbdc19e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f903293a973958a33cf0",
+    "url": "/static/js/2.b299e875.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.b299e875.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa2c87cd864a943e21b5",
+    "url": "/static/js/main.53d3884d.chunk.js"
+  },
+  {
+    "revision": "8f5431b127441a3e0cd9",
+    "url": "/static/js/runtime-main.c1ec362b.js"
+  },
+  {
+    "revision": "c8917bdd179237b4ffd72a6211cff1d0",
+    "url": "/static/media/logo.c8917bdd.png"
+  }
+]);
